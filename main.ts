7400ddd7d7d5511d16e82e617f25cf4adb87cefe
LPANHK76 <128042809+LPANHK76@users.noreply.github.com>
2023-03-20 10:48:53 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,62 @@
+let NOMBRE = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    NOMBRE = randint(1, 6)
+    if (NOMBRE == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (NOMBRE == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    }
+    if (NOMBRE == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    }
+    if (NOMBRE == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (NOMBRE == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (NOMBRE == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
